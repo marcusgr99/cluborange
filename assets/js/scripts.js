398980f651cb8d6f0e4f0c151227da0e87a96054
MathiasKullenberg <62767390+MathiasKullenberg@users.noreply.github.com>
2020-08-25 11:05:14 +0200

    Add files via upload

---
+++ assets/js/scripts.js
@@ -0,0 +1,176 @@
+
+(function ($) {
+    'use strict';
+
+    jQuery(document).ready(function () {
+
+        /*
+         * ----------------------------------------------------------------------------------------
+         *  PRELOADER JS
+         * ----------------------------------------------------------------------------------------
+         */
+        $(window).load(function () {
+            $('.preloader').fadeOut();
+            $('.preloader-area').delay(350).fadeOut('slow');
+        });
+
+        /*
+         * ----------------------------------------------------------------------------------------
+         *  CHANGE MENU BACKGROUND JS
+         * ----------------------------------------------------------------------------------------
+         */
+        $(window).on('scroll', function () {
+            if ($(window).scrollTop() > 200) {
+                $('.header-top-area').addClass('menu-bg');
+            } else {
+                $('.header-top-area').removeClass('menu-bg');
+            }
+        });
+
+
+
+        /*
+         * ----------------------------------------------------------------------------------------
+         *  SMOTH SCROOL JS
+         * ----------------------------------------------------------------------------------------
+         */
+
+        $('a.smoth-scroll').on("click", function (e) {
+            var anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $(anchor.attr('href')).offset().top - 50
+            }, 1000);
+            e.preventDefault();
+        });
+
+
+        /*
+         * ----------------------------------------------------------------------------------------
+         *  MAGNIFIC POPUP JS
+         * ----------------------------------------------------------------------------------------
+         */
+
+        var magnifPopup = function () {
+            $('.work-popup').magnificPopup({
+                type: 'image',
+                removalDelay: 300,
+                mainClass: 'mfp-with-zoom',
+                gallery: {
+                    enabled: true
+                },
+                zoom: {
+                    enabled: true, // By default it's false, so don't forget to enable it
+
+                    duration: 300, // duration of the effect, in milliseconds
+                    easing: 'ease-in-out', // CSS transition easing function
+
+                    // The "opener" function should return the element from which popup will be zoomed in
+                    // and to which popup will be scaled down
+                    // By defailt it looks for an image tag:
+                    opener: function (openerElement) {
+                        // openerElement is the element on which popup was initialized, in this case its <a> tag
+                        // you don't need to add "opener" option if this code matches your needs, it's defailt one.
+                        return openerElement.is('img') ? openerElement : openerElement.find('img');
+                    }
+                }
+            });
+        };
+        // Call the functions 
+        magnifPopup();
+        /*
+         * ----------------------------------------------------------------------------------------
+         *  PARALLAX JS
+         * ----------------------------------------------------------------------------------------
+         */
+
+        $(window).stellar({
+            responsive: true,
+            positionProperty: 'position',
+            horizontalScrolling: false
+        });
+
+        /*
+         * ----------------------------------------------------------------------------------------
+         *  COUNTER UP JS
+         * ----------------------------------------------------------------------------------------
+         */
+
+        $('.counter-num').counterUp();
+
+
+        /*
+         * ----------------------------------------------------------------------------------------
+         *  USER & SCREENSHOTS JS
+         * ----------------------------------------------------------------------------------------
+         */
+
+        $(".testimonial-list").owlCarousel({
+            items: 3,
+            autoPlay: true,
+            navigation: false,
+            itemsDesktop: [1199, 1],
+            itemsDesktopSmall: [980, 1],
+            itemsTablet: [768, 1],
+            itemsTabletSmall: false,
+            itemsMobile: [479, 1],
+            pagination: true,
+            autoHeight: true,
+        });
+
+        $(".app-list").owlCarousel({
+            items: 5,
+            autoPlay: true,
+            navigation: false,
+            itemsDesktop: [1199, 1],
+            itemsDesktopSmall: [980, 3],
+            itemsTablet: [768, 3],
+            itemsTabletSmall: false,
+            itemsMobile: [479, 1],
+            pagination: true,
+            autoHeight: true,
+        });
+
+
+        /*
+         * ----------------------------------------------------------------------------------------
+         *  EXTRA JS
+         * ----------------------------------------------------------------------------------------
+         */
+        $(document).on('click', '.navbar-collapse.in', function (e) {
+            if ($(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle') {
+                $(this).collapse('hide');
+            }
+        });
+        $('body').scrollspy({
+            target: '.navbar-collapse',
+            offset: 195
+        });
+
+        /*
+         * ----------------------------------------------------------------------------------------
+         *  SCROOL TO UP JS
+         * ----------------------------------------------------------------------------------------
+         */
+        $(window).scroll(function () {
+            if ($(this).scrollTop() > 250) {
+                $('.scrollup').fadeIn();
+            } else {
+                $('.scrollup').fadeOut();
+            }
+        });
+        $('.scrollup').on("click", function () {
+            $("html, body").animate({
+                scrollTop: 0
+            }, 800);
+            return false;
+        });
+
+        /*
+         * ----------------------------------------------------------------------------------------
+         *  WOW JS
+         * ----------------------------------------------------------------------------------------
+         */
+        new WOW().init();
+    });
+
+})(jQuery);
